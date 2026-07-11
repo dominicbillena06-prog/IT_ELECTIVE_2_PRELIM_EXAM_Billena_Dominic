7414ac7a3cbc863650d0b1489ca63c236fc851a5
DominicBillena <dominicbillena06@gmail.com>
2026-07-11 14:58:34 +0800

    meal by id

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetMealById.cs
@@ -1,24 +1,41 @@
-namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
+using System.Net;
+using System.Text.Json;
 
-// EXERCISE 3: GET Lookup by ID
-// TheMealDB API: https://themealdb.com/api/json/v1/1/lookup.php?i={id}
-//
-// Your task:
-// 1. Use the HttpClient to look up meal with ID 52772
-// 2. Assert status code is 200 OK
-// 3. Parse the JSON and assert the meal name is "Arrabiata"
-//
-// Note: TheMealDB meal IDs are numeric (52771 = Arrabiata)
+namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 public static class GetMealById
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=52771
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the meal name (strMeal) is "Arrabiata"
+        var url = "https://themealdb.com/api/json/v1/1/lookup.php?i=52771";
 
-        throw new NotImplementedException();
+        var response = await client.GetAsync(url);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception("Expected status code 200 OK.");
+        }
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument doc = JsonDocument.Parse(body);
+
+        var meals = doc.RootElement.GetProperty("meals");
+
+        if (meals.ValueKind == JsonValueKind.Null || meals.GetArrayLength() < 1)
+        {
+            throw new Exception("Meal not found.");
+        }
+
+        var mealName = meals[0]
+            .GetProperty("strMeal")
+            .GetString();
+
+        if (mealName != "Spicy Arrabiata Penne")
+        {
+            throw new Exception($"Expected Spicy Arrabiata Penne but got {mealName}.");
+        }
+
+        Console.WriteLine("Meal lookup successful.");
     }
 }
