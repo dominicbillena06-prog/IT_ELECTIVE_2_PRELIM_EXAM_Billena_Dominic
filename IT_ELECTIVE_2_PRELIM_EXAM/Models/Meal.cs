410d965a05e29576de64afea90a53a850a63567a
DominicBillena <dominicbillena06@gmail.com>
2026-07-11 13:41:50 +0800

    Exercise 1

--- IT_ELECTIVE_2_PRELIM_EXAM/Models/Meal.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM/Models/Meal.cs
@@ -1,28 +1,31 @@
 namespace IT_ELECTIVE_2_PRELIM_EXAM.Models;
 
-// EXERCISE 1: Encapsulation - Private Fields
-// The fields below are public. Your task:
-// - Make all fields PRIVATE
-// - Create public Properties with getters and setters
-// - The property names should match the field names (PascalCase)
-//
-// Currently, the properties below are STUBS that return wrong values.
-// Fix them to properly wrap the private fields.
-
 public class Meal
 {
-    public string name;
-    public string category;
-    public string area;
-    public string instructions;
-    public string thumbnail;
-    public string tags;
+    private string name;
+    private string category;
+    private string area;
+    private string instructions;
+    private string thumbnail;
+    private string tags;
 
-    // EXERCISE 1: Fix these stub properties to properly get/set from private fields
-    // After fixing, make the fields above PRIVATE
-    public string Name { get => ""; set { } }
-    public string Category { get => ""; set { } }
-    public string Area { get => ""; set { } }
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public string Category
+    {
+        get => category;
+        set => category = value;
+    }
+
+    public string Area
+    {
+        get => area;
+        set => area = value;
+    }
 
     public Meal()
     {
