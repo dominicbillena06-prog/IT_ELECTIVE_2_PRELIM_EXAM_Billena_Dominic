8e5a82602789bd73c9d039d5899f97c47fc8f3bd
DominicBillena <dominicbillena06@gmail.com>
2026-07-11 14:40:04 +0800

    Handle not found

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/HandleNotFound.cs
@@ -1,26 +1,35 @@
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 9: GET Handle 404 Not Found
 // TheMealDB API: https://themealdb.com/api/json/v1/1/lookup.php?i={id}
-//
-// Your task:
-// 1. Use the HttpClient to look up a meal with an ID that doesn't exist (ID 999999)
-// 2. Assert the HTTP status code is 200 OK (TheMealDB always returns 200)
-// 3. Parse the JSON and assert that the "meals" field is null
-//    (meaning no meal was found for that ID)
-//
-// This teaches: APIs can return 200 OK but still indicate "not found"
-// in the response body via null data.
 
 public static class HandleNotFound
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/lookup.php?i=999999
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert that "meals" field is null (not found)
+        var url = "https://themealdb.com/api/json/v1/1/lookup.php?i=999999";
 
-        throw new NotImplementedException();
+        var response = await client.GetAsync(url);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception("Expected status code 200 OK.");
+        }
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument doc = JsonDocument.Parse(body);
+
+        var meals = doc.RootElement.GetProperty("meals");
+
+        if (meals.ValueKind != JsonValueKind.Null)
+        {
+            throw new Exception("Expected meals to be null.");
+        }
+
+        Console.WriteLine("Not found response handled correctly.");
     }
 }
