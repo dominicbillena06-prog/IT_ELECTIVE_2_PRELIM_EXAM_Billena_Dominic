e91b01d4916a4a2e4874c8301033e4ca55cbe277
DominicBillena <dominicbillena06@gmail.com>
2026-07-11 13:56:14 +0800

    Exercise 4

--- IT_ELECTIVE_2_PRELIM_EXAM/Models/Category.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM/Models/Category.cs
@@ -1,20 +1,21 @@
 namespace IT_ELECTIVE_2_PRELIM_EXAM.Models;
 
+
 public class Category
 {
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
 
- 
+  
     public Category()
     {
         Name = "";
         Description = "";
     }
-
-
     public Category(string name, string description)
     {
+        Name = name;
+        Description = description;
     }
 
     public override string ToString()
