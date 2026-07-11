0ad020a5f52af9c16e07801ab50cd3210449b30f
DominicBillena <dominicbillena06@gmail.com>
2026-07-11 14:26:50 +0800

    Implement random meal retrieval

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/GetRandomMeal.cs
@@ -1,26 +1,29 @@
+using System.Net;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
-// EXERCISE 1: GET Random Meal
-// TheMealDB API: https://themealdb.com/api/json/v1/1/random.php
-//
-// Your task:
-// 1. Use the HttpClient to send a GET request to the URL above
- // 2. Read the response as a string
-// 3. Assert that the status code is 200 OK
-// 4. Assert that the response body is not null or empty
-//
-// Hint: Use await client.GetAsync(url) then check response.StatusCode
-// Hint: Use await response.Content.ReadAsStringAsync() to get the body
-
-public static class GetRandomMeal
+public class GetRandomMeal
 {
-    public static async Task Run(System.Net.Http.HttpClient client)
+    public async Task Run()
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/random.php
-        // TODO: Read the response content as a string
-        // TODO: Assert status code is 200 OK
-        // TODO: Assert response body is not null or empty
+        using var client = new HttpClient();
 
-        throw new NotImplementedException();
+        var url = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+        var response = await client.GetAsync(url);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception("Request failed.");
+        }
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        if (string.IsNullOrEmpty(body))
+        {
+            throw new Exception("Response body is empty.");
+        }
+
+        Console.WriteLine("Random meal fetched successfully.");
     }
 }
