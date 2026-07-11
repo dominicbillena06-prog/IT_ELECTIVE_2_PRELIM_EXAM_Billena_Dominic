67a5c39f3edf5938ce851ca69cd6739dae3ed563
DominicBillena <dominicbillena06@gmail.com>
2026-07-11 15:02:37 +0800

    exercise 5 filtr by ingridients

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/FilterByIngredient.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/FilterByIngredient.cs
@@ -1,24 +1,35 @@
+using System.Net;
+using System.Text.Json;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 5: GET Filter by Ingredient
 // TheMealDB API: https://themealdb.com/api/json/v1/1/filter.php?i={ingredient}
-//
-// Your task:
-// 1. Use the HttpClient to filter meals by ingredient "chicken_breast"
-// 2. Assert status code is 200 OK
-// 3. Parse the JSON and assert the "meals" array has at least 1 item
-//
-// Response format: { "meals": [{ "strMeal": "...", "strMealThumb": "...", "idMeal": "..." }, ...] }
 
 public static class FilterByIngredient
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/filter.php?i=chicken_breast
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the "meals" array has at least 1 item
+        var url = "https://themealdb.com/api/json/v1/1/filter.php?i=chicken_breast";
 
-        throw new NotImplementedException();
+        var response = await client.GetAsync(url);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new Exception("Expected status code 200 OK.");
+        }
+
+        var body = await response.Content.ReadAsStringAsync();
+
+        using JsonDocument doc = JsonDocument.Parse(body);
+
+        var meals = doc.RootElement.GetProperty("meals");
+
+        if (meals.ValueKind == JsonValueKind.Null || meals.GetArrayLength() < 1)
+        {
+            throw new Exception("No meals found for this ingredient.");
+        }
+
+        Console.WriteLine("Ingredient filter successful.");
     }
 }
